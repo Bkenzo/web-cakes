d9bb3226f24f71d4fe41e2d9b63e0376dd557bbf
Bruno Kenzo <brunokenzzo@gmail.com>
2020-10-01 20:26:48 -0300

    adicionando responsividade a empresa

--- empresa.php
+++ empresa.php
@@ -1,20 +1,13 @@
 <div class="container-fluid somos">
     <div class="row">
-        <div class="col text-center">
+        <div class="col-sm-6 col-12 text-center">
             <img src="bolo-branco.png" alt="">
             <p>Adipisicing id amet et ad id. Culpa voluptate laborum ut aliqua quis eiusmod do amet dolor ipsum est qui occaecat commodo. Do commodo irure labore Lorem consectetur veniam. Dolore cillum magna do ipsum enim eiusmod est labore et labore sint.</p>
             Excepteur fugiat id dolor excepteur officia. Incididunt labore non irure duis laboris velit do non. Nisi velit ad et consectetur magna nostrud ipsum irure.
         </div>
     </div>
 </div>
-
 <br>
 <br>
 <br>
-<div class="container-fluid spikes">
-    <div class="row">
-        <div class="col-12 text-center">
-            <h1>Quem Somos</h1>
-        </div>
-    </div>  
-</div>
+<br>
